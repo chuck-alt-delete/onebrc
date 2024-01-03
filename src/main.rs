cec9c24670d0488eaa216307140fcdbbb84f2dd8
chuck-alt-delete <chuck@materialize.com>
2024-01-02 16:36:11 -0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,124 @@
+use std::time::Instant;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, BufReader, BufRead};
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+struct WeatherData {
+    min_temp: f64,
+    max_temp: f64,
+    total_temp: f64,
+    count: u64,
+}
+
+impl WeatherData {
+    fn new() -> WeatherData {
+        WeatherData {
+            min_temp: f64::INFINITY,
+            max_temp: f64::NEG_INFINITY,
+            total_temp: 0.0,
+            count: 0,
+        }
+    }
+
+    fn update(&mut self, temp: f64) {
+        self.min_temp = self.min_temp.min(temp);
+        self.max_temp = self.max_temp.max(temp);
+        self.total_temp += temp;
+        self.count += 1;
+    }
+
+    fn combine(&mut self, other: &WeatherData) {
+        self.min_temp = self.min_temp.min(other.min_temp);
+        self.max_temp = self.max_temp.max(other.max_temp);
+        self.total_temp += other.total_temp;
+        self.count += other.count;
+    }
+}
+
+fn process_chunk(chunk: Vec<String>) -> HashMap<String, WeatherData> {
+    let mut results = HashMap::new();
+    for line in chunk {
+        let parts: Vec<&str> = line.split(';').collect();
+        if parts.len() != 2 {
+            continue;
+        }
+        let station = parts[0].to_string();
+        let temp: f64 = parts[1].parse().unwrap_or_default();
+        
+        results.entry(station)
+               .or_insert_with(WeatherData::new)
+               .update(temp);
+    }
+    results
+}
+
+fn main() -> io::Result<()> {
+    let timer = Instant::now();
+    let path = "/Users/chuck/scratch/onebrc/resources/measurements.csv";
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let chunk_size = 1_000_000; // Number of lines per chunk
+    let mut chunk = Vec::with_capacity(chunk_size);
+    let mut handles = vec![];
+    let final_results = Arc::new(Mutex::new(HashMap::new()));
+
+    for line in reader.lines() {
+        let line = line?;
+        chunk.push(line);
+
+        if chunk.len() == chunk_size {
+            let final_results_clone = Arc::clone(&final_results);
+            let chunk_to_process = std::mem::replace(&mut chunk, Vec::with_capacity(chunk_size));
+
+            let handle = thread::spawn(move || {
+                let chunk_results = process_chunk(chunk_to_process);
+                let mut final_res = final_results_clone.lock().unwrap();
+                for (station, data) in chunk_results {
+                    final_res.entry(station)
+                             .or_insert_with(WeatherData::new)
+                             .combine(&data);
+                }
+            });
+            handles.push(handle);
+        }
+    }
+
+    // Process the last chunk if it's not empty
+    if !chunk.is_empty() {
+        let final_results_clone = Arc::clone(&final_results);
+        let chunk_to_process = chunk;
+
+        let handle = thread::spawn(move || {
+            let chunk_results = process_chunk(chunk_to_process);
+            let mut final_res = final_results_clone.lock().unwrap();
+            for (station, data) in chunk_results {
+                final_res.entry(station)
+                         .or_insert_with(WeatherData::new)
+                         .combine(&data);
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    let final_res = final_results.lock().unwrap();
+    for (station, data) in final_res.iter() {
+        println!("{}: min = {:.1}, max = {:.1}, mean = {:.1}", 
+                 station, 
+                 data.min_temp, 
+                 data.max_temp, 
+                 data.total_temp / data.count as f64);
+    }
+    let duration = timer.elapsed();
+    print!("Time elapsed: {:?}", duration);
+
+    Ok(())
+}
+
+
---
+++ tools/create_csv.rs
@@ -0,0 +1,60 @@
+use rand::{distributions::{Distribution, Uniform}, Rng};
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let stations = [
+        "Hamburg", "Bulawayo", "Palembang", "St. John's", "Cracow", "Bridgetown", 
+        "Istanbul", "Roseau", "Conakry", "Nuuk", "Kabul", "Tirana", "Algiers", 
+        "Pago Pago", "Andorra la Vella", "Luanda", "The Valley", "Saint John's", 
+        "Buenos Aires", "Yerevan", "Oranjestad", "Canberra", "Vienna", "Baku", 
+        "Nassau", "Manama", "Dhaka", "Bridgetown", "Minsk", "Brussels", "Belmopan", 
+        "Porto-Novo", "Hamilton", "Thimphu", "Sucre", "Kralendijk", "Sarajevo", 
+        "Gaborone", "Brasilia", "Diego Garcia", "Bandar Seri Begawan", "Sofia", 
+        "Ouagadougou", "Bujumbura", "Praia", "Phnom Penh", "Douala", "Ottawa", 
+        "Praia", "Bangui", "N'Djamena", "Santiago", "Beijing", "Flying Fish Cove", 
+        "West Island", "Bogota", "Moroni", "Avarua", "Zagreb", "Havana", "Willemstad", 
+        "Nicosia", "Prague", "Copenhagen", "Djibouti", "Roseau", "Santo Domingo",
+        "Quito", "Cairo", "San Salvador", "Malabo", "Asmara", "Tallinn", "Addis Ababa", 
+        "Stanley", "Torshavn", "Suva", "Helsinki", "Paris", "Cayenne", "Papeete", 
+        "Libreville", "Banjul", "Tbilisi", "Berlin", "Accra", "Gibraltar", "Athens", 
+        "Nuuk", "St. George's", "Basse-Terre", "Hagatna", "Guatemala City", "St. Peter Port", 
+        "Conakry", "Bissau", "Georgetown", "Port-au-Prince", "Tegucigalpa", "Budapest", 
+        "Reykjavik", "New Delhi", "Jakarta", "Tehran", "Baghdad", "Dublin", "Douglas", 
+        "Jerusalem", "Rome", "Kingston", "Tokyo", "Saint Helier", "Amman", "Astana", 
+        "Nairobi", "Tarawa Atoll", "Pyongyang", "Seoul", "Pristina", "Kuwait City", 
+        "Bishkek", "Vientiane", "Riga", "Beirut", "Maseru", "Monrovia", "Tripoli", 
+        "Vaduz", "Vilnius", "Luxembourg", "Skopje", "Antananarivo", "Lilongwe", "Kuala Lumpur", 
+        "Male", "Bamako", "Valletta", "Majuro", "Fort-de-France", "Nouakchott", "Port Louis", 
+        "Mamoudzou", "Mexico City", "Chisinau", "Monaco", "Ulaanbaatar", "Podgorica", 
+        "Plymouth", "Rabat", "Maputo", "Naypyidaw", "Windhoek", "Yaren", "Kathmandu", 
+        "Amsterdam", "Noumea", "Wellington", "Managua", "Niamey", "Abuja", "Alofi", 
+        "Kingston", "Saipan", "Oslo", "Muscat", "Islamabad", "Melekeok", "Panama City", 
+        "Port Moresby", "Asuncion", "Lima", "Manila", "Adamstown", "Warsaw", "Lisbon", 
+        "San Juan", "Doha", "Brazzaville", "Bucharest", "Moscow", "Kigali", "Gustavia", 
+        "Jamestown", "Basseterre", "Castries", "Marigot", "Saint-Pierre", "Kingstown", 
+        "Apia", "San Marino", "Sao Tome", "Riyadh", "Dakar", "Belgrade", "Victoria", 
+        "Freetown", "Singapore", "Philipsburg", "Bratislava", "Ljubljana", "Honiara", 
+        "Mogadishu", "Pretoria", "Grytviken", "Juba", "Madrid", "Colombo", "Khartoum", 
+        "Paramaribo", "Longyearbyen", "Mbabane", "Stockholm", "Bern", "Damascus", "Taipei", 
+        "Dushanbe", "Dar es Salaam", "Bangkok", "Lome", "Nuku'alofa", "Port of Spain", 
+        "Tunis", "Ankara", "Ashgabat", "Grand Turk", "Funafuti", "Kampala", "Kiev", 
+        "Abu Dhabi", "London", "Washington, D.C.", "Montevideo", "Tashkent", "Port Vila", 
+        "Vatican City", "Caracas", "Hanoi", "Mata-Utu", "El Aaiun", "Sana'a", "Lusaka", "Harare"
+    ];
+    
+    let file = File::create("/Users/chuck/scratch/onebrc/resources/measurements.csv")?;
+    let mut writer = BufWriter::new(file);
+    let mut rng = rand::thread_rng();
+    let station_dist = Uniform::from(0..stations.len());
+    let temp_dist = Uniform::from(-30.0..45.0);
+
+    for _ in 0..1_000_000_000 {
+        let station = stations[station_dist.sample(&mut rng)];
+        let temp = temp_dist.sample(&mut rng);
+        writeln!(writer, "{};{:.1}", station, temp)?;
+    }
+
+    Ok(())
+}
